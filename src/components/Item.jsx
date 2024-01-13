e59eec7e84e9197331f27dde192d169ff6bd9bad
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-01-13 12:42:03 -0500

    feat: add remove from card button

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { bagAction } from "../store/bagSlice";
 
 function Item({ item }) {
+  const bag = useSelector((store) => store.bag);
+  const elementFound = bag.indexOf(item.id) >= 0;
+
   const dispatch = useDispatch();
   const handleAdd = () => {
     dispatch(bagAction.addInitialItems(item.id));
+  };
+
+  const handleRemove = () => {
+    dispatch(bagAction.removeFromBag(item.id));
   };
   return (
     <div className="item-container">
@@ -19,9 +26,15 @@ function Item({ item }) {
         <span className="original-price">Rs {item.original_price}</span>
         <span className="discount">({item.discount_percentage}% OFF)</span>
       </div>
-      <button className="btn-add-bag" onClick={handleAdd}>
-        Add to Bag
-      </button>
+      {elementFound ? (
+        <button className="btn-add-bag btn-remove" onClick={handleRemove}>
+          Remove
+        </button>
+      ) : (
+        <button className="btn-add-bag" onClick={handleAdd}>
+          Add to Bag
+        </button>
+      )}
     </div>
   );
 }
